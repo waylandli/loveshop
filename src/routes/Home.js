4e5ddad558b0ef8afed26394e8517468e7937d94
= <=>
2021-08-13 08:56:52 -0700

    Fixed issue with bottom border, added a header for home, and added some styling.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import Navbar from '../components/Navbar/Navbar';
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 const Home = (props) => {
-    return ( 
-        <div>
-            <Navbar/>
-            This is the Home
-        </div>
-     );
-}
- 
+  const styles = {
+    header: {
+      textAlign: "center",
+    },
+  };
+  return (
+    <div>
+      <Navbar />
+      <div style={styles.header}>
+        <em>
+          <h2>Love's Auto</h2>
+        </em>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
